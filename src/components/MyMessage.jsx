346dcd7fff2e83bf03768faa4230b6845b52050f
Álef de Souza Rosa <alefsouzamac1023@gmail.com>
2022-01-03 11:39:02 -0300

    commit

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -11,7 +11,7 @@ const MyMessage = ({ message }) => {
   }
 
   return (
-    <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+    <div className="message" style={{ float: 'right', marginRight: '18px', color: '#222629', backgroundColor: '#e0e0e0' }}>
       {message.text}
     </div>
   );
